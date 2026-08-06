e861911d5d24dc97fa65372687d1a675c5ce35b5
Bob Marley <40630774+khadafigans@users.noreply.github.com>
2026-08-06 22:22:49 +0700

    Update run.cmd

--- BOT_AWS/run.cmd
+++ BOT_AWS/run.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd /d D:\Data\BOT\bot_aws\BOT_AWS
-start cmd /k "python BOT_AWS.py C:\Users\KHADAPI GANS\Documents\Penting\SES\GENIP\hasil\ips.txt"
-start cmd /k "python BOT_AWS.py C:\Users\KHADAPI GANS\Documents\Penting\SES\GENIP\hasil\domains.txt"
+start cmd /k "python BOT_AWS.py C:\Users\PATH\SES\GENIP\hasil\ips.txt"
+start cmd /k "python BOT_AWS.py C:\Users\PATH\SES\GENIP\hasil\domains.txt"
